9f7c79ccea62b47e849c7bbe4ee4fbdb79b7e412
Lucas Mouilleron <lucas.mouilleron@me.com>
2015-05-26 18:13:55 +0200

    clean clean

--- ___stupid/admin/clean.php
+++ ___stupid/admin/clean.php
@@ -6,8 +6,10 @@ $stupidBackend = new stupidBackend();
 $stupidBackend->lockPage();
 
 /////////////////////////////////////////////////////////////////////////////
-$deletedContents = $stupidBackend->cleanContents();
-$deletedFiles = $stupidBackend->cleanFiles();
+if(isset($_GET["do"])) {
+	$deletedContents = $stupidBackend->cleanContents();
+	$deletedFiles = $stupidBackend->cleanFiles();
+}
 
 ?>
 
@@ -15,20 +17,30 @@ $deletedFiles = $stupidBackend->cleanFiles();
 
 <div class="container">
 
-	<?php if(count($deletedContents) == 0):?>
-		<div class="alert alert-success" role="alert">No contents to delete !</div>
-	<?php else:?>
-		<?php foreach ($deletedContents as $deletedContent) :?>
-			<div class="alert alert-success" role="alert">Content <strong><?php echo $deletedContent?></strong> deleted</div>
-		<?php endforeach;?>
+	<div class="alert alert-danger" role="alert">
+		<p><strong>Beware !</strong></p>
+		<p>Are you sure you want to delete unreferenced files or contents ?</p>
+		<p><a class="btn btn-warning" href="?do">Yes, proceed !</a></p>
+	</div>
+
+	<?php if(isset($deletedContents)):?>
+		<?php if(count($deletedContents) == 0):?>
+			<div class="alert alert-success" role="alert">No contents to delete !</div>
+		<?php else:?>
+			<?php foreach ($deletedContents as $deletedContent) :?>
+				<div class="alert alert-success" role="alert">Content <strong><?php echo $deletedContent?></strong> deleted</div>
+			<?php endforeach;?>
+		<?php endif;?>
 	<?php endif;?>
 
-	<?php if(count($deletedFiles) == 0):?>
-		<div class="alert alert-success" role="alert">No files to delete !</div>
-	<?php else:?>
-		<?php foreach ($deletedFiles as $deletedFile) :?>
-			<div class="alert alert-success" role="alert">File <strong><?php echo $deletedFile?></strong> deleted</div>
-		<?php endforeach;?>
+	<?php if(isset($deletedFiles)):?>
+		<?php if(count($deletedFiles) == 0):?>
+			<div class="alert alert-success" role="alert">No files to delete !</div>
+		<?php else:?>
+			<?php foreach ($deletedFiles as $deletedFile) :?>
+				<div class="alert alert-success" role="alert">File <strong><?php echo $deletedFile?></strong> deleted</div>
+			<?php endforeach;?>
+		<?php endif;?>
 	<?php endif;?>
 
 </div>
